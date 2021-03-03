0a9032a985e9d63519f213f45452cc3133ed2207
rx-rx <79772634+rx-rx@users.noreply.github.com>
2021-03-03 00:31:30 -0600

    Create 6-Sum-Square-Difference.go

---
+++ Golang/6-Sum-Square-Difference.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var number float64
+	number=100
+	sum:=math.Pow((number*(number+1))/2, 2)
+	var i float64
+	for i=1; i<=number; i++{
+	  sum-=i*i
+	}
+	fmt.Println(sum)
+}
